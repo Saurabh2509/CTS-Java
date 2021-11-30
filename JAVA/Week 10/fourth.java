229d17b03f46d4ddaca5f3e7c52d9d1ef044c733
Abhijeet Kumar Ghosh <60868332+abhijeet49@users.noreply.github.com>
2021-11-30 18:45:21 +0530

    Create fourth.java

---
+++ JAVA/Week 10/fourth.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        double r= 7.5, area;
+        area= r*r*3.14;
+        System.out.println("Area is = "+area);
+	}
+}
